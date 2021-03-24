50078a9690096ff6954d5613e4bfba701e371165
Francois Huyghe <francois@13milliseconds.com>
2021-03-24 16:10:48 -0400

    Show chamber and ditrict on posts page

--- web/app/themes/couragescore2021/app/admin.php
+++ web/app/themes/couragescore2021/app/admin.php
@@ -22,3 +22,38 @@ add_action('customize_register', function (\WP_Customize_Manager $wp_customize) 
 add_action('customize_preview_init', function () {
     wp_enqueue_script('sage/customizer.js', asset_path('scripts/customizer.js'), ['customize-preview'], null, true);
 });
+
+/***************************** */
+// Show the Chamber in admin menu
+/***************************** */
+/*
+ * Add columns to exhibition post list
+ */
+function add_acf_columns ( $columns ) {
+    return array_merge ( $columns, array ( 
+      'senate_or_assembly' => __ ( 'Chamber' ),
+      'party'   => __ ( 'Party' ) 
+    ) );
+  }
+add_filter ( 'manage_people_posts_columns', __NAMESPACE__ . '\\add_acf_columns' );
+/*
+ * Add columns to exhibition post list
+ */
+function people_custom_column ( $column, $post_id ) {
+    switch ( $column ) {
+      case 'senate_or_assembly':
+            $chamber = get_field ( 'senate_or_assembly', $post_id );
+            $district = get_field ( 'district', $post_id );
+            if($district){
+                echo $chamber . ' district ' . $district;
+            } else {
+                echo 'No District';
+            }
+        break;
+        case 'party':
+            echo get_field ( 'party', $post_id );
+        break;
+    }
+  }
+add_action ( 'manage_people_posts_custom_column', __NAMESPACE__ . '\\people_custom_column', 10, 2 );
+
