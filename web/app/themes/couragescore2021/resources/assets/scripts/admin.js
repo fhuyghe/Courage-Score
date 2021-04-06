83a0f45a7a9ae11310a820c94e96617f0af73c71
Francois Huyghe <francois@13milliseconds.com>
2021-04-06 09:51:03 -0400

    fixed admin.js

--- web/app/themes/couragescore2021/resources/assets/scripts/admin.js
+++ web/app/themes/couragescore2021/resources/assets/scripts/admin.js
@@ -1,7 +1,7 @@
 console.log('The Real Admin JS');
 
 $('#loadSubmit').on('click', function () {
-    $scoreID = $('#scorecards').val();
+    let $scoreID = $('#scorecards').val();
     console.log($scoreID);
     //get_bills_by_scorecard
 });
