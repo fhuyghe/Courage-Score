8a18d2c5cf9dfb9676fa72cdb90a2f728637c6d7
Francois Huyghe <francois@13milliseconds.com>
2021-04-13 18:22:19 -0400

    social links in options

--- web/app/themes/couragescore2021/resources/views/partials/social-links.blade.php
+++ web/app/themes/couragescore2021/resources/views/partials/social-links.blade.php
@@ -1,4 +1,5 @@
 <div class="social">
-    <a href="facebook"><i class="fab fa-facebook-f"></i></a>
-    <a href="twitter"><i class="fab fa-twitter"></i></a>
+    <a href="{{ get_field('facebook', 'options') }}" target="_blank"><i class="fab fa-facebook-f"></i></a>
+    <a href="{{ get_field('twitter', 'options') }}" target="_blank"><i class="fab fa-twitter"></i></a>
+    <a href="{{ get_field('instagram,', 'options') }}" target="_blank"><i class="fab fa-instagram"></i></a>
 </div>
