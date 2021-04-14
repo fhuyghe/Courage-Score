f29798dee85ee343cc73126b88e0f1abce70eaff
Francois Huyghe <francois@13milliseconds.com>
2021-04-14 11:50:21 -0400

    desktop square on lading page

--- web/app/themes/couragescore2021/resources/views/front-page.blade.php
+++ web/app/themes/couragescore2021/resources/views/front-page.blade.php
@@ -3,11 +3,42 @@
 @section('content')
 
   <section id="top">
+    <div class="squares-top">
+      @for ($i = 0; $i < 8; $i++)
+        <div class="square" id="square-{{ $i }}"></div>
+      @endfor
+    </div>
+    <div class="squares-left">
+      <div>
+      <div class="square"></div>
+      <div class="square"></div>
+      </div>
+      <div>
+      <div class="square"></div>
+      <div class="square"></div>
+      </div>
+  </div>
     <div class="content">
       <h1>{!! App::title() !!}</h1>
       <p>Paragraph</p>
       @include('partials.search')
       <a href="/all-representatives">See all representatives</a>
+    </div>
+      <div class="squares-right">
+        <div>
+          <div class="square"></div>
+          <div class="square"></div>
+          </div>
+          <div>
+          <div class="square"></div>
+          <div class="square"></div>
+          </div>
+      </div>
+      <div class="squares-bottom">
+        @for ($i = 0; $i < 8; $i++)
+      <div class="square" id="square-{{ $i }}"></div>
+    @endfor
+      </div>
   </section>
 
   <section id="searchResults">
