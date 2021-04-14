ea39a2fb50eb96303960c8e69eeb162015cc6b75
Francois Huyghe <francois@13milliseconds.com>
2021-04-14 11:59:14 -0400

    Add Contributions settings

--- web/app/themes/couragescore2021/app/setup.php
+++ web/app/themes/couragescore2021/app/setup.php
@@ -173,6 +173,12 @@ if( function_exists('acf_add_options_page') ) {
         'menu_title'    => 'Footer',
         'parent_slug'   => 'theme-general-settings',
     ));
+
+    acf_add_options_sub_page(array(
+        'page_title'    => 'Contributions Settings',
+        'menu_title'    => 'Contributions',
+        'parent_slug'   => 'theme-general-settings',
+    ));
     
 }
 
