703f40d622e58a7dc59d95d024fd7ce61c19f27c
Francois Huyghe <francois@13milliseconds.com>
2021-04-15 19:09:37 -0400

    Footer Signup form

--- web/app/themes/couragescore2021/resources/views/partials/footer.blade.php
+++ web/app/themes/couragescore2021/resources/views/partials/footer.blade.php
@@ -5,12 +5,14 @@
         <div class="logo">
           <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
         </div>
+        @include('partials.contact-links')
         <div class="copyright">
         @php dynamic_sidebar('sidebar-footer') @endphp
         </div>
       </div>
       <div class="col-md-4">
-        @include('partials.contact-links')
+        <h3>{{ get_field('footer_newsletter_title', 'option') }}</h3>
+        @include('partials.newsletter-form')
         @include('partials.social-links')
       </div>
     </div>
