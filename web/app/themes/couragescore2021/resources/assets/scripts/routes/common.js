e5a9c0c609308b5e141e0f977ad0f09298175f36
Francois Huyghe <francois@13milliseconds.com>
2021-04-16 10:44:34 -0400

    move search to landing page

--- web/app/themes/couragescore2021/resources/assets/scripts/routes/common.js
+++ web/app/themes/couragescore2021/resources/assets/scripts/routes/common.js
@@ -1,15 +1,7 @@
-import autocomplete from 'autocompleter';
-
 export default {
   init() {
 
-    let isHome = $('.home').length > 0 ? true : false;
-
     // JavaScript to be fired on all pages
-    $('.address-button').on('click', function () {
-      const address = $('#address-input').val();
-        getDistrict(address);
-    });
 
     //Hmaburger
     $('.hamburger').on('click', function () {
@@ -17,89 +9,6 @@ export default {
       $('.nav-wrap').toggleClass('active');
     });
 
-    var getDistrict = function (address) {
-      if (isHome) {
-        // Get the reps here
-      $('#address-input').attr('disabled', true);
-      $('.search-container.address').addClass('loading');
-
-      $.ajax({
-        // eslint-disable-next-line no-undef
-        url : ajax_object.ajax_url,
-        data : {
-          action: 'get_district_ajax',
-          address,
-        },
-        success: function (response) {
-          console.log(response);
-          let searchResults = document.getElementById('searchResults');
-
-          //Populate address
-          $('#address').html(address);
-
-          // Populate Assembly
-          $('#assemblyRep').html(response.data[0]);
-          
-          // Populate Senate
-          $('#senateRep').html(response.data[1]);
-          
-          searchResults.classList.add('active');
-          searchResults.scrollIntoView({ block: 'start', behavior: 'smooth' });
-          
-          //Remove Loading Status
-          $('#address-input').attr('disabled', false);
-          $('.search-container.address').removeClass('loading');
-        },
-      });
-      } else {
-        //Forward to the search results
-        window.location.assign('/my-representatives?address=' + encodeURIComponent(address));
-      }
-    }
-
-    /*****************/
-    // Search by Address
-    /*****************/
-    var addressInput = document.getElementById('address-input');
-
-    autocomplete({
-      input: addressInput,
-      highlightMatches: true,
-      className: 'suggestions',
-      render: function(item) {
-        var div = document.createElement('div');
-        div.textContent = item.text;
-        return div;
-      },
-      fetch: function(text, update) {
-          text = text.toLowerCase();
-
-          $.ajax({
-            url: 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/suggest?',
-            data: {
-              f: 'json',
-              text: text,
-              category: 'Address, Residence, Postal',
-              countryCode: 'usa',
-              searchExtent: '-125.656774,31.611563,-113.850027,42.427281',
-            },
-            cache: true,
-          })
-            .done(function (res) {
-              let suggestions = res.suggestions;
-              suggestions = suggestions.filter(function (address) { return address.text.includes('CA,') });
-              update(suggestions);
-            });
-      },
-      onSelect: function(item) {
-        addressInput.value = item.text;
-        getDistrict(item.text);
-      },
-      customize: function(input, inputRect, container) {
-        container.style.top = inputRect.top - 80;
-        container.style.maxHeight = '200px';
-      },
-    });
     
   },
   finalize() {
--- web/app/themes/couragescore2021/resources/assets/scripts/routes/home.js
+++ web/app/themes/couragescore2021/resources/assets/scripts/routes/home.js
@@ -12,6 +12,11 @@ export default {
       $('#top .search-container').removeClass('active');
       $('#top .search-container.' + this.value).addClass('active');
     })
+
+    $('.address-button').on('click', function () {
+      const address = $('#address-input').val();
+        getDistrict(address);
+    });
     
     /*****************/
     // Search by Name
@@ -47,6 +52,86 @@ export default {
         window.location.href = item.url;
         },
     });
+
+    /*****************/
+    // Search by Address
+    /*****************/
+    var addressInput = document.getElementById('address-input');
+
+    autocomplete({
+      input: addressInput,
+      highlightMatches: true,
+      className: 'suggestions',
+      render: function(item) {
+        var div = document.createElement('div');
+        div.textContent = item.text;
+        return div;
+      },
+      fetch: function(text, update) {
+          text = text.toLowerCase();
+
+          $.ajax({
+            url: 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/suggest?',
+            data: {
+              f: 'json',
+              text: text,
+              category: 'Address, Residence, Postal',
+              countryCode: 'usa',
+              searchExtent: '-125.656774,31.611563,-113.850027,42.427281',
+            },
+            cache: true,
+          })
+            .done(function (res) {
+              let suggestions = res.suggestions;
+              suggestions = suggestions.filter(function (address) { return address.text.includes('CA,') });
+              update(suggestions);
+            });
+      },
+      onSelect: function(item) {
+        addressInput.value = item.text;
+        getDistrict(item.text);
+      },
+      customize: function(input, inputRect, container) {
+        container.style.top = inputRect.top - 80;
+        container.style.maxHeight = '200px';
+      },
+    });
+
+    var getDistrict = function (address) {
+        // Get the reps here
+      $('#address-input').attr('disabled', true);
+      $('.search-container.address').addClass('loading');
+
+      $.ajax({
+        // eslint-disable-next-line no-undef
+        url : ajax_object.ajax_url,
+        data : {
+          action: 'get_district_ajax',
+          address,
+        },
+        success: function (response) {
+          console.log(response);
+          let searchResults = document.getElementById('searchResults');
+
+          //Populate address
+          $('#address').html(address);
+
+          // Populate Assembly
+          $('#assemblyRep').html(response.data[0]);
+          
+          // Populate Senate
+          $('#senateRep').html(response.data[1]);
+          
+          searchResults.classList.add('active');
+          searchResults.scrollIntoView({ block: 'start', behavior: 'smooth' });
+          
+          //Remove Loading Status
+          $('#address-input').attr('disabled', false);
+          $('.search-container.address').removeClass('loading');
+        },
+      });
+    }
+
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
