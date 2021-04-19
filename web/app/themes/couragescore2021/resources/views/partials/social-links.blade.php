3acd12c1493ec63d34b8f461d9516841c5e0e93a
Francois Huyghe <francois@13milliseconds.com>
2021-04-19 10:10:48 -0400

    IG link fixed

--- web/app/themes/couragescore2021/resources/views/partials/social-links.blade.php
+++ web/app/themes/couragescore2021/resources/views/partials/social-links.blade.php
@@ -1,5 +1,5 @@
 <div class="social">
-    <a href="{{ get_field('facebook', 'options') }}" target="_blank"><i class="fab fa-facebook-f"></i></a>
-    <a href="{{ get_field('twitter', 'options') }}" target="_blank"><i class="fab fa-twitter"></i></a>
-    <a href="{{ get_field('instagram,', 'options') }}" target="_blank"><i class="fab fa-instagram"></i></a>
+    <a href="{{ get_field('facebook', 'option') }}" target="_blank"><i class="fab fa-facebook-f"></i></a>
+    <a href="{{ get_field('twitter', 'option') }}" target="_blank"><i class="fab fa-twitter"></i></a>
+    <a href="{{ get_field('instagram', 'option') }}" target="_blank"><i class="fab fa-instagram"></i></a>
 </div>
