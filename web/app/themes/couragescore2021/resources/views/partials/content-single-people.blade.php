bf0f208d253c13cf361b46e4accc70c8a3d6fa9c
Francois Huyghe <francois@13milliseconds.com>
2021-04-19 22:24:45 -0400

    grader adjustments and maps

--- web/app/themes/couragescore2021/resources/views/partials/content-single-people.blade.php
+++ web/app/themes/couragescore2021/resources/views/partials/content-single-people.blade.php
@@ -21,14 +21,16 @@ $senateAssembly = get_field('senate_or_assembly');
           <div class="rep-name-title">
             <h3>
               {!! get_the_title($post->ID) !!}
-              @include('partials.grade-display')
             </h3>
             <h4>State <span class="body">{{ get_field('senate_or_assembly', $post->ID) }}</span></h4>
             @php $leadership = get_field('leadership_position', $post->ID) @endphp
             @if($leadership)
-                <h4 class="leadership">{{ $leadership }}</h4>
+            <h4 class="leadership">{{ $leadership }}</h4>
             @endif
-        </div> 
+          </div> 
+          <div class="grade-wrap">
+            @include('partials.grade-display')
+          </div>
           <div class="portrait">
           <div class="portrait-wrap">
             {!! get_the_post_thumbnail( $post->ID, 'thumbnail' );  !!}
