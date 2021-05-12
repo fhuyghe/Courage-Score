d44e6a48ba3e74e251e3769ac2dda71d7436a8f5
Francois Huyghe <francois@13milliseconds.com>
2021-05-12 11:30:06 -0400

    partners without logos

--- web/app/themes/couragescore2021/resources/views/partials/content-page-partners.blade.php
+++ web/app/themes/couragescore2021/resources/views/partials/content-page-partners.blade.php
@@ -50,30 +50,16 @@ endforeach;
     {!! get_field('partners_without_scorecards') !!}
 </section>
     
-    <section>
-        <div class="row">
+    <section id="partnersNoLogo">
+        <ul>
             @foreach ($withoutLink as $post)
             @php setup_postdata($post) @endphp
-            <div class="partner col-6 col-md-3">
+            <li>
                 <a href="{{ get_field('website') }}" target="blank">
-                    @if( has_post_thumbnail() )
-                    {!! the_post_thumbnail('medium') !!}
-                    @else
-                    <h3>{{ the_title() }}</h3>
-                    @endif
-                    </a>
-
-                    @php $scorecard_link = get_field('scorecard_link') @endphp
-                    @if($scorecard_link)
-                    <p>
-                        <a href="{{ $scorecard_link }}" target="blank">
-                            Scorecard
-                        </a>
-                    </p>
-                    @endif
-                </div>
+                    {{ the_title() }}
+                </a>
+            </li>
                 @php wp_reset_postdata($post) @endphp
             @endforeach
-        </div>
-    </div>
+        </ul>
 </section>
