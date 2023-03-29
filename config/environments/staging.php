a88b0c8b124f844c30e405436fef25aa670e65aa
Francois Huyghe <francois@13millieconds.com>
2023-03-29 13:19:44 -0400

    no debug in staging

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -15,6 +15,6 @@ use Roots\WPConfig\Config;
  */
 
 Config::define('DISALLOW_INDEXING', true);
-Config::define('WP_DEBUG', true);
-Config::define('WP_DEBUG_DISPLAY', true);
-Config::define('WP_DEBUG_LOG', true);
+// Config::define('WP_DEBUG', true);
+// Config::define('WP_DEBUG_DISPLAY', true);
+// Config::define('WP_DEBUG_LOG', true);
