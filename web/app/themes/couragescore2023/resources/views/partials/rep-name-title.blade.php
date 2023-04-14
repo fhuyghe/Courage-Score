62c05d4b67984dbb053a99219a833d509312c0f2
Francois Huyghe <francois@13millieconds.com>
2023-04-14 16:03:06 -0400

    Add former for old legislators

--- web/app/themes/couragescore2023/resources/views/partials/rep-name-title.blade.php
+++ web/app/themes/couragescore2023/resources/views/partials/rep-name-title.blade.php
@@ -5,7 +5,7 @@
         </a>
         @include('partials.grade-display')
     </h3>
-    <h4>State <span class="body">{{ get_field('senate_or_assembly', $post->ID) }}</span></h4>
+    <h4><span>@if(has_category('out-of-office',$post->ID))Former @endif</span>State <span class="body">{{ get_field('senate_or_assembly', $post->ID) }}</span></h4>
     @php $leadership = get_field('leadership_position', $post->ID) @endphp
     @if($leadership)
         <h4 class="leadership">{{ $leadership }}</h4>
